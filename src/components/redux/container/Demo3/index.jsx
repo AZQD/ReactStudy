49f999bcca8c434bf8ed48a2966dfc53b0d7cef4
AZQD <604972395@qq.com>
2019-04-18 15:29:58 +0800

    Demo3

--- src/components/redux/container/Demo3/index.jsx
+++ src/components/redux/container/Demo3/index.jsx
@@ -8,7 +8,7 @@ import * as actionFN from '../../action'
 class CateList extends Component{
   
   componentDidMount(){
-    console.log(123, this.props);
+    console.log('Demo3-props', this.props);
     // ajax.getJSON('https://suggest.taobao.com/sug?code=utf-8&q=%E5%8C%97%E4%BA%AC')
     ajax.getJSON('https://ewxtongzhen.58.com/shop/posts/category/b/list')
       .then((data) => {
@@ -22,7 +22,7 @@ class CateList extends Component{
   render() {
     return (
       <div>
-        <CommonMenu history={this.props.history} />
+        {/*<CommonMenu history={this.props.history} />*/}
         <div className="demo3Box">
           666
         </div>
