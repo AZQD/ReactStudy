615fb9aa969d72c20626aac979237e06b17646b6
AZQD <604972395@qq.com>
2019-07-12 01:35:19 +0800

    redux基础demo

--- src/components/index/redux/demo02.jsx
+++ src/components/index/redux/demo02.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-const Demo01 = (()=>{
+
+import {createStore} from 'redux'
+
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'REDUCE':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+let store = createStore(reducer);
+
+const ctrlFun = (flag) => {
+  store.dispatch({
+    type: flag ? 'ADD' : 'REDUCE'
+  });
+}
+
+
+const Demo01 = (() => {
   return (
     <div>
-      demo02.jsx
+      <button onClick={() => ctrlFun(true)}>ADD</button>
+      <span>{store.getState()}</span>
+      <button onClick={() => ctrlFun(false)}>REDUCE</button>
     </div>
   )
 });
-
+store.subscribe(() => {
+  console.log(store.getState());
+})
 export default Demo01;
