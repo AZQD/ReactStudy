41512cfbd5ca37be6b63b59f7b1b1b5e738ec1e9
AZQD <604972395@qq.com>
2019-09-07 21:10:43 +0800

    引入css-to-react-native-transform并测试

--- src/components/index/transform/Transform1.jsx
+++ src/components/index/transform/Transform1.jsx
@@ -1,5 +1,51 @@
 import React from 'react'
 import {render} from 'react-dom'
+import transform from "css-to-react-native-transform";
+console.log(34, transform);
+
+let test1 = transform(`
+.myClass {
+  font-size: 18px;
+  line-height: 24px;
+  color: red;
+  float: right;
+  a: 1;
+  b: 2;
+  c-d: 3;
+}
+.test::before{
+    content: 'hello'
+}
+.other {
+  padding: 1rem;
+}
+`, { parseAllSelectors: true });
+
+let test2 = transform(`
+  .foo {
+    color: #f00;
+  }
+
+  :export {
+    myProp: #fff;
+  }
+`);
+
+let test3 = transform(
+  `
+  .container {
+    background-color: #f00;
+  }
+
+  @media (orientation: landscape) {
+    .container {
+      background-color: #00f;
+    }
+  }
+`,
+  { parseMediaQueries: true },
+);
+console.log('编译后的样式：', test3);
 
 export default class Transform1 extends React.Component{
   render (){
