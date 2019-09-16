915788ae4ca32e8ff7dc7c5dd99fbcdbc25daf5f
AZQD <604972395@qq.com>
2019-09-16 11:50:35 +0800

    项目优化

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");//清除相关文件
 
 module.exports = {
   entry: {
-    test: __dirname + "/src/components/test/Index.jsx",//已多次提及的唯一入口文件
-    index: __dirname + "/src/components/index/Index.jsx",//已多次提及的唯一入口文件
-    redux: __dirname + "/src/components/redux/Index.jsx",//已多次提及的唯一入口文件
+    index: __dirname + "/src/pages/index/Index.jsx",//已多次提及的唯一入口文件
+    redux: __dirname + "/src/pages/redux/Index.jsx",//已多次提及的唯一入口文件,
+    test: __dirname + "/src/pages/test/Index.jsx",//已多次提及的唯一入口文件
   },
   resolve: {
     extensions: ['.js', ".css", '.jsx', '.less']//自动补全文件后缀
