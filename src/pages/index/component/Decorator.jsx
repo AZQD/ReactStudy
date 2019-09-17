35da088a5d6ea4d5d47f5921ab484383a15a2250
AZQD <604972395@qq.com>
2019-09-17 15:12:48 +0800

    方法的装饰

--- src/pages/index/component/Decorator.jsx
+++ src/pages/index/component/Decorator.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 // 1.类的装饰
-function decorator1(target) { // 装饰器函数的第一个参数，就是所要装饰的目标类
+/*function decorator1(target) { // 装饰器函数的第一个参数，就是所要装饰的目标类
   console.log(target);
   target.param1 = 1; // 为类添加一个静态属性
   target.prototype.param2 = 2; // 添加实例属性
@@ -22,6 +22,23 @@ export default class Component1 extends React.Component {
   }
 }
 console.log('组件的静态属性：', Component1.param1);
-console.log('组件的静态属性：', Component1.param2);
+console.log('组件的静态属性：', Component1.param2);*/
 
-// 2.类的装饰
+
+// 2.方法的装饰
+
+function decorator2(target, name, descriptor) {
+  console.log(target, name, descriptor);
+}
+export default class Component2 extends React.Component{
+  constructor(porps){
+    super(porps);
+  }
+  @decorator2
+  addFun(a, b){
+    return a + b;
+  }
+  render(){
+    return <div>类的装饰和方法的装饰</div>
+  }
+}
