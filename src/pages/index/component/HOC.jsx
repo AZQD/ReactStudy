45cebc2666e34fc4ee0f4bca3a69dc9f6564ef8c
AZQD <604972395@qq.com>
2019-09-17 14:12:11 +0800

    初始化高阶组件

--- src/pages/index/component/HOC.jsx
+++ src/pages/index/component/HOC.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 export default class HOC extends React.Component{
-
-  @testtable
-  test(){}
   render(){
     return <div>12345</div>
   }
-}
-
-function testtable(target, b, c) {
-  console.log(target, b,c);
 }
