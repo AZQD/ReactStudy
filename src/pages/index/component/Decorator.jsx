ac75b3e39cf2a8f263440bf39329e84bc8a4b46a
AZQD <604972395@qq.com>
2019-09-17 14:52:30 +0800

    类的装饰

--- src/pages/index/component/Decorator.jsx
+++ src/pages/index/component/Decorator.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-export default class HOC extends React.Component{
 
-  @testtable
-  test(){}
-  render(){
-    return <div>12345</div>
+// 1.类的装饰
+function decorator1(target) { // 装饰器函数的第一个参数，就是所要装饰的目标类
+  console.log(target);
+  target.param1 = 1; // 为类添加一个静态属性
+  target.prototype.param2 = 2; // 添加实例属性
+}
+
+@decorator1
+export default class Component1 extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  componentDidMount(){
+    console.log('组件的实例属性：', this.param1);
+    console.log('组件的实例属性：', this.param2);
+  }
+  render() {
+    return <div>类的装饰和方法的装饰</div>
   }
 }
+console.log('组件的静态属性：', Component1.param1);
+console.log('组件的静态属性：', Component1.param2);
 
-function testtable(target, b, c) {
-  console.log(target, b,c);
-}
+// 2.类的装饰
--- src/pages/index/component/HOC.jsx
+++ src/pages/index/component/HOC.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default class HOC extends React.Component{
   render(){
-    return <div>12345</div>
+    return <div>HOC</div>
   }
 }
