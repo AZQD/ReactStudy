daa2ea462aaecacb29074d1790fea3b8925c5f3f
AZQD <604972395@qq.com>
2019-09-18 18:08:08 +0800

    css转RN样式流程

--- src/pages/index/component/csstree/CssTree.jsx
+++ src/pages/index/component/csstree/CssTree.jsx
@@ -1,112 +1,39 @@
 import React from 'react'
-import csstree from 'css-tree' // 引入CSS语法树
 import cssCollect from './cssCollect'
 import rnCollect from './rnCollect'
+import {csstreeParse, getCurlyBracesPropertyArr, checkProperty, humpFun} from './util'
 
-// 传入的花括号样式
+
+// 1.传入花括号样式（后续使用node工具的fs读取文件）
 let entryCurlyBracesCss = '{color: red; width: 12px; border: 1px solid red; line-height: 16px;}';
+console.log('第一步：传入花括号样式：', entryCurlyBracesCss);
 
-// 花括号内CSS转义为语法树
-let curlyBracesCssAST = csstree.parse(entryCurlyBracesCss, {
-  context: 'block'
-});
 
-// 获取属性数组
+// 2.花括号内CSS转义为语法树
+let curlyBracesCssAST = csstreeParse(entryCurlyBracesCss);
+console.log('第二步：花括号内CSS转义为语法树：', curlyBracesCssAST);
+
+
+// 3.通过语法树，获取属性数组
 let propertyArr = getCurlyBracesPropertyArr(curlyBracesCssAST.children.head);
-console.log('获取属性数组', propertyArr);
+console.log('第三步：通过语法树，获取属性数组：', propertyArr);
 
-// 检测CSS结果：true：属性都支持；false：有不支持的属性，具体看console；
+
+// 4.检测CSS支持结果：true：属性都支持；false：有不支持的属性，具体看console；
 let checkCSSResult = checkProperty(propertyArr, cssCollect);
-console.log('检测结果：', checkCSSResult);
+console.log('第四步：检测CSS支持结果：', `W3School 的 CSS 参考手册校验${checkCSSResult ? '通过' : '未通过'}`);
 
-// checkResult如果是true，则转换驼峰，并校验是否是RN支持的属性；
-if(checkCSSResult){
-  console.log('准备转RN');
+
+// 检测CSS支持结果如果是true，则转换驼峰，并校验是否是RN支持的属性；
+if (checkCSSResult) {
+  // 5.css属性转换驼峰属性
   let humpPropertyArr = propertyArr.map((item) => humpFun(item));
-  console.log(111, humpPropertyArr);
-  // 检测RN结果：true：属性都支持；false：有不支持的属性，具体看console；
+  console.log('第五步：转换驼峰：', humpPropertyArr);
+
+
+  // 6.检测RN主持结果：true：属性都支持；false：有不支持的属性，具体看console；
   let checkRNResult = checkProperty(humpPropertyArr, rnCollect);
-  console.log('检测结果：', checkRNResult);
-}
-
-/**
- * CSS属性字符串转换为驼峰命名
- * eg: humpFun('ab-cd-e'); // abCdE
- * @param str
- * @returns {string}
- */
-function humpFun(str) {
-  let arr = str.split('-');
-  let newArr = [];
-  arr.map((item, index) => {
-    if (index) {
-      newArr.push(item.charAt(0).toUpperCase() + item.slice(1));
-    } else {
-      newArr.push(item);
-    }
-  });
-  return newArr.join('');
-}
-
-// 判断是否为CSS可支持的属性
-function checkProperty(targetArr, collectArr) {
-  if(!targetArr.length){
-    console.log('传入数组为空，请校验！');
-    return;
-  }
-  let flagArr = []; // 检查结果
-  // 遍历数组，确定是否是可支持的属性；
-  for (let i = 0; i < targetArr.length; i++) {
-    let targetItem = targetArr[i];
-    let flag = false;
-    for (let j = 0; j < collectArr.length; j++) {
-      let collectItem = collectArr[j];
-      if(targetItem === collectItem){
-        flag = true;
-        break;
-      }
-    }
-    flagArr.push(flag);
-  }
-  console.log(flagArr);
-
-  // 打印属性支持情况，并返回结果（默认返回true，如有不支持的属性，则返回false）
-  let returnFlag = true;
-  for(let i = 0; i<flagArr.length; i++){
-    let flagItem = flagArr[i];
-      if(flagItem){
-        console.log(`${targetArr[i]}属性可用；`);
-      }else{
-        returnFlag = false;
-        console.log(`${targetArr[i]}属性不可用；`);
-      }
-  }
-  return returnFlag;
-}
-
-// 使用递归函数，遍历语法树，将属性合并成数组，并返回；
-function getCurlyBracesPropertyArr(params) {
-  if (typeof params !== 'object') {
-    console.log('语法树格式有误，请检查格式！');
-    return;
-  }
-  if (params === null) { // 为空返回空数组，避免报错；
-    return [];
-  }
-  const {data, next} = params;
-  if (data) {
-    console.log('data', data);
-    if (next) {
-      console.log('next', next);
-      return [data.property].concat(getCurlyBracesPropertyArr(next)); // 递归函数
-    } else {
-      console.log('没有next');
-      return [data.property];
-    }
-  } else {
-    console.log('没有data');
-    return [];
-  }
+  console.log('第六步：检测RN支持结果：', `React Native Styling Cheat Sheet校验${checkRNResult ? '通过' : '未通过'}`);
 }
 
 
