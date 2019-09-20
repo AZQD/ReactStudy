52121ffe594b7e15cb62056d1aa0fe03bd709bec
AZQD <604972395@qq.com>
2019-09-20 21:21:58 +0800

    封装的 CSS 转 RN 的函数和测试

--- src/pages/index/component/AST/entryCss.js
+++ src/pages/index/component/AST/entryCss.js
@@ -1,2 +1,43 @@
+import {
+  postcssParse,
+  postcssASTToPropValueArr,
+  getCheckedPropArr,
+  humpHandle
+} from './utils/util'
 
-export const entryCss = '{color: red; line-height: 16px; border: 1px solid red; float: right; a: 1; background: red;}';
+
+export const entryCss = '{color: red; line-height: 16px; border: 1px solid red; float: right; ab-cd: 12px;}';
+
+
+console.log('----------测试 begin------------');
+
+
+// 测试：
+let CSSToRNtransformResult = CSSTransformRN(entryCss);
+console.log(' CSS 转 RN 结果：', CSSToRNtransformResult);
+
+
+/**
+ * 封装的 CSS 转 RN 的函数
+ * @param entryCss
+ * @constructor
+ */
+function CSSTransformRN(entryCss) {
+  let postcssAST = postcssParse(entryCss);
+  let propValueArr = postcssASTToPropValueArr(postcssAST);
+  let {totalPropArr: cssTotalPropArr, passedPropArr: cssPassedPropArr, notPassedPropArr: cssNotPassedPropArr} = getCheckedPropArr(propValueArr, 'postcss', 'css');
+  let humpPropArr = cssTotalPropArr.map((item) => {
+    return {
+      ...item,
+      prop: humpHandle(item.prop)
+    }
+  });
+  let {totalPropArr: RNTotalPropArr, passedPropArr: RNPassedPropArr, notPassedPropArr: RNNotPassedPropArr} = getCheckedPropArr(humpPropArr, 'postcss', 'rn');
+  console.log('RNTotalPropArr', RNTotalPropArr);
+  console.log('RNPassedPropArr', RNPassedPropArr);
+  console.log('RNNotPassedPropArr', RNNotPassedPropArr);
+  return {RNTotalPropArr, RNPassedPropArr, RNNotPassedPropArr};
+}
+
+
+console.log('----------测试 end------------');
