2f6285d14b6a00dbc2262f84b37eab01d15d6fda
AZQD <604972395@qq.com>
2019-09-20 16:43:59 +0800

    csstree参数优化

--- src/pages/index/component/AST/postcss/Postcss.jsx
+++ src/pages/index/component/AST/postcss/Postcss.jsx
@@ -2,54 +2,55 @@ import React from 'react'
 import postcss from 'postcss'
 import cssCollect from '../utils/cssCollect'
 import rnCollect from '../utils/rnCollect'
-import {csstreeParse, getCurlyBracesPropertyArr, checkProperty, humpFun} from '../utils/util'
-import classNames from 'classnames';
+import {
+  postcssParse,
+  postcssASTToPropValueArr,
+  getCheckedPropArr,
+  humpHandle
+} from '../utils/util'
+
+
+console.log('--------postcss begin----------');
+
 
 // 1.传入花括号样式（后续使用node工具的fs读取文件）
-let entryCurlyBracesCss = '{color: red; width: 12px; border: 1px solid red; line-height: 16px; font-size: 16px; float: right; a: 1;}';
-console.log('第一步：传入花括号样式：', entryCurlyBracesCss);
+let entryCss = '{color: red; width: 12px; border: 1px solid red; line-height: 16px; font-size: 16px; float: right; a: 1;}';
+console.log('第一步：传入花括号样式：', entryCss);
 
-// 2.花括号内CSS转义为语法树(postcss)，遍历语法树，获取属性prop和value数组
-let propValueArr = getCurlyBracesPropValueArr(entryCurlyBracesCss);
+
+// 2.花括号内CSS转义为语法树(postcss)
+let postcssAST = postcssParse(entryCss);
+console.log('第二步：花括号内CSS转义为语法树：', postcssAST);
+
+
+// 3.通过语法树，获取属性prop和value数组
+let propValueArr = postcssASTToPropValueArr(postcssAST);
+console.log('第三步：通过语法树，获取属性数组：', propValueArr);
 
 
 // 4.检测CSS支持结果：true：属性都支持；false：有不支持的属性，具体看console；
-// let {checkFlagArr: checkFlagCSSArr, checkReturnFlag: checkCSSResult} = checkProperty(propertyArr, cssCollect);
-// console.log('第四步：检测CSS支持结果：', `W3School 的 CSS 参考手册校验${checkCSSResult ? '通过' : '未通过'}`);
-
-postcssCheckProperty(propValueArr);
-function postcssCheckProperty(propValueArr) {
-  let propArr = propValueArr.map((item => item.prop));
-  let aa = checkProperty(propArr, cssCollect);
-}
+let {totalPropArr: cssTotalPropArr, passedPropArr: cssPassedPropArr, notPassedPropArr: cssNotPassedPropArr} = getCheckedPropArr(propValueArr, cssCollect);
+console.log('第四步：检测CSS支持结果：', `W3School 的 CSS 参考手册校验${!cssNotPassedPropArr.length ? '通过' : '未通过'}`);
 
 
+// 检测CSS支持结果如果是true，则转换驼峰，并校验是否是RN支持的属性；
+// 目前不管是否是CSS支持的，都直接执行下一步；
+// 5.css属性转换驼峰属性
+let humpPropArr = cssTotalPropArr.map((item) => {
+  return {
+    ...item,
+    prop: humpHandle(item.prop)
+  }
+});
+console.log('第五步：转换驼峰：', humpPropArr);
 
 
+// 6.检测RN主持结果：true：属性都支持；false：有不支持的属性，具体看console；
+let {totalPropArr: RNTotalPropArr, passedPropArr: RNPassedPropArr, notPassedPropArr: RNNotPassedPropArr} = getCheckedPropArr(humpPropArr, rnCollect);
+console.log('第六步：检测RN支持结果：', `React Native Styling Cheat Sheet校验${!RNNotPassedPropArr.length ? '通过' : '未通过'}`);
 
 
-
-
-
-/**
- * 花括号内CSS转义为语法树(postcss)
- * 遍历语法树，获取属性prop和value数组
- * @param entryCurlyBracesCss
- * @returns {Array}
- */
-function getCurlyBracesPropValueArr(entryCurlyBracesCss) {
-  let root = postcss.parse(entryCurlyBracesCss);
-  console.log('postcss语法树：', root);
-  let propValueArr = [];
-  root.nodes && root.nodes.length>0 && root.nodes[0].nodes.map((item) => {
-    propValueArr.push({prop: item.prop, value: item.value});
-  });
-  console.log('解析后返回数组：', propValueArr);
-  return propValueArr;
-}
-
-
-
+console.log('--------postcss end----------');
 
 
 export default class Postcss extends React.Component {
