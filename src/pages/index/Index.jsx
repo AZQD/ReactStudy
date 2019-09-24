823dff808a95bdd63e2c80d10b3a98f27b5124a5
AZQD <604972395@qq.com>
2019-09-24 20:41:55 +0800

    首页更新

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import {render} from "react-dom";
-import {HashRouter as Router, Route, Link, Switch} from "react-router-dom";
-import PartLeft from "./PartLeft";
-import PartRight from "./PartRight";
-import "../../../static/js/public.js";
-import "../../../static/css/public.css";
-import "../../css/index/index.less";
+import React from 'react';
+import {render} from 'react-dom';
+import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import PartLeft from './PartLeft';
+import PartRight from './PartRight';
+import '../../../static/js/public.js';
+import '../../../static/css/public.css';
+import '../../css/index/index.less';
 // import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import hashHistory from "react-dom";
+// import hashHistory from 'react-dom';
 // console.log("hashHistory",hashHistory);
 
 export default class Index extends React.Component {
-	render() {
-		return (
-			<Router>
-				<div className="wrapper">
-					<div className="partLeft">
-						<PartLeft/>
-					</div>
-					<div className="partRight">
-						<PartRight/>
-					</div>
-				</div>
-			</Router>
-		);
-	}
+  render () {
+    return (
+      <Router>
+        <div className="wrapper">
+          <div className="partLeft">
+            <PartLeft/>
+          </div>
+          <div className="partRight">
+            <PartRight/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
-render(<Index/>, document.getElementById("box"));
+render(<Index/>, document.getElementById('box'));
