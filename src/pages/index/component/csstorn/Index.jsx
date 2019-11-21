eb13cb27eeb52ccdd31cadcb679eeca04a045d85
AZQD <604972395@qq.com>
2019-11-21 23:33:16 +0800

    界面更新

--- src/pages/index/component/csstorn/Index.jsx
+++ src/pages/index/component/csstorn/Index.jsx
@@ -108,6 +108,20 @@ console.log('transform:', transform([
 import React from 'react'
 import {getCheckedPropArr, humpHandle, postcssASTToPropValueArr, postcssParse} from "../AST/utils/util";
 
+import {rnCollectFilter, rnCollectFilterAbbrevia} from '../AST/utils/rnCollectFilter'
+import marginAbbrev from './abbreviate/margin';
+import paddingAbbrev from './abbreviate/padding';
+import borderAbbrev from './abbreviate/border';
+import borderLeftAbbrev from './abbreviate/borderLeft';
+import borderRightAbbrev from './abbreviate/borderRight';
+import borderTopAbbrev from './abbreviate/borderTop';
+import borderBottomAbbrev from './abbreviate/borderBottom';
+import backgroundAbbrev from './abbreviate/background';
+import boxShadowAbbrev from './abbreviate/boxShadow';
+import texShadowAbbrev from './abbreviate/texShadow';
+import textDecorationAbbrev from './abbreviate/textDecoration';
+import flexAbbrev from './abbreviate/flex';
+
 
 console.log('----------css-to-react-native begin------------');
 
@@ -131,116 +145,134 @@ console.log('----------css-to-react-native end------------');
 
 console.log('----------纯粹处理转RN begin------------');
 
-let entryCss = '.container{text-decoration: underline solid blue; text-shadow: 1px 2px 3px red; box-shadow: 1px 2px 3px 4px .5;background: yellows; border-left: 1px solid red; padding: 10px 20px; margin: 12px 20px 15px 34px; color: red; line-height: 16px; border: 1px solid red; float: right; ab-cd: 12px;}';
+function transformSelf (entryCss) {
+  // let entryCss = '.container{text-decoration: underline solid blue; text-shadow: 1px 2px 3px red; box-shadow: 1px 2px 3px 4px .5;background: yellows; border-left: 1px solid red; padding: 10px 20px; margin: 12px 20px 15px 34px; color: red; line-height: 16px; border: 1px solid red; float: right; ab-cd: 12px;}';
 
 // 1.传入花括号样式（后续使用node工具的fs读取文件）
-console.log('第一步：传入花括号样式：', entryCss);
+  console.log('第一步：传入花括号样式：', entryCss);
 
 // 2.花括号内CSS转义为语法树(postcss)
-let postcssAST = postcssParse(entryCss);
-console.log('第二步：花括号内CSS转义为语法树：', postcssAST);
+  let postcssAST = postcssParse(entryCss);
+  console.log('第二步：花括号内CSS转义为语法树：', postcssAST);
 
 // 3.通过语法树，获取属性prop和value数组
-let propValueArr = postcssASTToPropValueArr(postcssAST);
-console.log('第三步：通过语法树，获取属性数组：', propValueArr);
+  let propValueArr = postcssASTToPropValueArr(postcssAST);
+  console.log('第三步：通过语法树，获取属性数组：', propValueArr);
 
 // 4.检测CSS支持结果：true：属性都支持；false：有不支持的属性，具体看console；
-let {totalPropArr: cssTotalPropArr, passedPropArr: cssPassedPropArr, notPassedPropArr: cssNotPassedPropArr} = getCheckedPropArr(propValueArr, 'postcss', 'css');
-console.log('第四步：检测CSS支持结果：', `W3School 的 CSS 参考手册校验${!cssNotPassedPropArr.length ? '通过' : '未通过'}`);
-console.log('第四步：检测CSS支持结果：', cssTotalPropArr);
+  let {totalPropArr: cssTotalPropArr, passedPropArr: cssPassedPropArr, notPassedPropArr: cssNotPassedPropArr} = getCheckedPropArr(propValueArr, 'postcss', 'css');
+  console.log('第四步：检测CSS支持结果：', `W3School 的 CSS 参考手册校验${!cssNotPassedPropArr.length ? '通过' : '未通过'}`);
+  console.log('第四步：检测CSS支持结果：', cssTotalPropArr);
 
 // 检测CSS支持结果如果是true，则转换驼峰，并校验是否是RN支持的属性；
 // 目前不管是否是CSS支持的，都直接执行下一步；
 // 5.css属性转换驼峰属性
-let humpPropArr = cssTotalPropArr.map((item) => {
-  return {
-    ...item,
-    prop: humpHandle(item.prop)
-  }
-});
-console.log('第五步：转换驼峰：', humpPropArr);
-
-
-import {rnCollectFilter, rnCollectFilterAbbrevia} from '../AST/utils/rnCollectFilter'
-import marginAbbrev from './abbreviate/margin';
-import paddingAbbrev from './abbreviate/padding';
-import borderAbbrev from './abbreviate/border';
-import borderLeftAbbrev from './abbreviate/borderLeft';
-import borderRightAbbrev from './abbreviate/borderRight';
-import borderTopAbbrev from './abbreviate/borderTop';
-import borderBottomAbbrev from './abbreviate/borderBottom';
-import backgroundAbbrev from './abbreviate/background';
-import boxShadowAbbrev from './abbreviate/boxShadow';
-import texShadowAbbrev from './abbreviate/texShadow';
-import textDecorationAbbrev from './abbreviate/textDecoration';
-import flexAbbrev from './abbreviate/flex';
-
-
-function handlePropValue (humpPropArr) {
-  if (!humpPropArr.length) {
-    console.log('传入数组为空，请校验！');
-    return false;
-  }
-  let passedPropArr = []; // 通过的数组
-  let notPassedPropArr = []; // 未通过的数组
-  // 遍历数组，确定是否是可支持的属性；item示例：{prop: "color", value: "red"}
-  humpPropArr.map((item, index) => {
-    item.value = item.value.split(' ').map(valueItem => {
-      if (/\dpx$/.test(valueItem)) { // 单位处理!!!(可以扩展处理rem)思路：value值先split，正则处理每一块，然后再join；
-        return parseFloat(valueItem);
-      } else {
-        return valueItem;
-      }
-    }).join(' ');
-    if (rnCollectFilter.includes(item.prop)) {// js判断某个值是否在rnCollect数组中
-      if (rnCollectFilterAbbrevia.includes(item.prop)) {// js判断某个值是否在简写数组中
-        console.log('RN支持的样式，且要处理简写的有：', item);
-        let valueArr = item.value.split(' ');
-        let abbrevArr = [
-          {type: 'margin', abbrevFun: marginAbbrev},
-          {type: 'padding', abbrevFun: paddingAbbrev},
-          {type: 'border', abbrevFun: borderAbbrev},
-          {type: 'borderLeft', abbrevFun: borderLeftAbbrev},
-          {type: 'borderRight', abbrevFun: borderRightAbbrev},
-          {type: 'borderTop', abbrevFun: borderTopAbbrev},
-          {type: 'borderBottom', abbrevFun: borderBottomAbbrev},
-          {type: 'background', abbrevFun: backgroundAbbrev},
-          {type: 'boxShadow', abbrevFun: boxShadowAbbrev},
-          {type: 'textShadow', abbrevFun: texShadowAbbrev},
-          {type: 'textDecoration', abbrevFun: textDecorationAbbrev},
-          {type: 'flex', abbrevFun: flexAbbrev},
-        ];
-        abbrevArr.map(abbrevItem => {
-          if (item.prop === abbrevItem.type) {
-            passedPropArr.push.apply(passedPropArr, abbrevItem.abbrevFun(valueArr));
-          }
-        });
-      } else {
-        console.log('RN支持的样式有：', item);
-        passedPropArr.push(item);
-      }
-    } else {
-      console.log('不是RN支持的样式有：', item);
-      notPassedPropArr.push(item);
+  let humpPropArr = cssTotalPropArr.map((item) => {
+    return {
+      ...item,
+      prop: humpHandle(item.prop)
     }
   });
+  console.log('第五步：转换驼峰：', humpPropArr);
 
-  const totalPropArr = passedPropArr.concat(notPassedPropArr);
-  console.log('最终结果passedPropArr：', passedPropArr);
-  console.log('最终结果notPassedPropArr：', notPassedPropArr);
-  console.log('最终结果的数组：', totalPropArr);
-  return {totalPropArr, passedPropArr, notPassedPropArr};
+
+
+  function handlePropValue (humpPropArr) {
+    if (!humpPropArr.length) {
+      console.log('传入数组为空，请校验！');
+      return false;
+    }
+    let passedPropArr = []; // 通过的数组
+    let notPassedPropArr = []; // 未通过的数组
+    // 遍历数组，确定是否是可支持的属性；item示例：{prop: "color", value: "red"}
+    humpPropArr.map((item, index) => {
+      item.value = item.value.split(' ').map(valueItem => {
+        if (/\dpx$/.test(valueItem)) { // 单位处理!!!(可以扩展处理rem)思路：value值先split，正则处理每一块，然后再join；
+          return parseFloat(valueItem);
+        } else {
+          return valueItem;
+        }
+      }).join(' ');
+      if (rnCollectFilter.includes(item.prop)) {// js判断某个值是否在rnCollect数组中
+        if (rnCollectFilterAbbrevia.includes(item.prop)) {// js判断某个值是否在简写数组中
+          console.log('RN支持的样式，且要处理简写的有：', item);
+          let valueArr = item.value.split(' ');
+          let abbrevArr = [
+            {type: 'margin', abbrevFun: marginAbbrev},
+            {type: 'padding', abbrevFun: paddingAbbrev},
+            {type: 'border', abbrevFun: borderAbbrev},
+            {type: 'borderLeft', abbrevFun: borderLeftAbbrev},
+            {type: 'borderRight', abbrevFun: borderRightAbbrev},
+            {type: 'borderTop', abbrevFun: borderTopAbbrev},
+            {type: 'borderBottom', abbrevFun: borderBottomAbbrev},
+            {type: 'background', abbrevFun: backgroundAbbrev},
+            {type: 'boxShadow', abbrevFun: boxShadowAbbrev},
+            {type: 'textShadow', abbrevFun: texShadowAbbrev},
+            {type: 'textDecoration', abbrevFun: textDecorationAbbrev},
+            {type: 'flex', abbrevFun: flexAbbrev},
+          ];
+          abbrevArr.map(abbrevItem => {
+            if (item.prop === abbrevItem.type) {
+              passedPropArr.push.apply(passedPropArr, abbrevItem.abbrevFun(valueArr));
+            }
+          });
+        } else {
+          console.log('RN支持的样式有：', item);
+          passedPropArr.push(item);
+        }
+      } else {
+        console.log('不是RN支持的样式有：', item);
+        notPassedPropArr.push(item);
+      }
+    });
+
+    const totalPropArr = passedPropArr.concat(notPassedPropArr);
+    console.log('最终结果passedPropArr：', passedPropArr);
+    console.log('最终结果notPassedPropArr：', notPassedPropArr);
+    console.log('最终结果的数组：', totalPropArr);
+    return {totalPropArr, passedPropArr, notPassedPropArr};
+  }
+
+  // 6.检测RN主持结果：true：属性都支持；false：有不支持的属性，具体看console；
+  let {totalPropArr: RNTotalPropArr, passedPropArr: RNPassedPropArr, notPassedPropArr: RNNotPassedPropArr} = handlePropValue(humpPropArr);
+  return {RNTotalPropArr, RNPassedPropArr, RNNotPassedPropArr};
+
 }
-
-// 6.检测RN主持结果：true：属性都支持；false：有不支持的属性，具体看console；
-let {totalPropArr: RNTotalPropArr, passedPropArr: RNPassedPropArr, notPassedPropArr: RNNotPassedPropArr} = handlePropValue(humpPropArr);
 
 
 
 console.log('----------纯粹处理转RN end------------');
+
+
+
 import './index.less';
 export default class Index extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      entryCss: '',
+      RNTotalPropArr: [],
+      RNPassedPropArr: [],
+      RNNotPassedPropArr: [],
+    };
+  }
+
+  componentWillMount () {
+    let entryCss = '.container{text-decoration: underline solid blue; text-shadow: 1px 2px 3px red; box-shadow: 1px 2px 3px 4px .5;background: yellows; border-left: 1px solid red; padding: 10px 20px; margin: 12px 20px 15px 34px; color: red; line-height: 16px; border: 1px solid red; float: right; ab-cd: 12px;}';
+    this.transformFun(entryCss);
+  }
+
+  transformFun(entryCss){
+    let {RNTotalPropArr, RNPassedPropArr, RNNotPassedPropArr} = transformSelf(entryCss);
+    this.setState({
+      RNTotalPropArr,
+      RNPassedPropArr,
+      RNNotPassedPropArr
+    });
+  };
+
   render() {
+    let {RNTotalPropArr, RNPassedPropArr, RNNotPassedPropArr} = this.state;
     return <div className="cssToRNBox">
       {/*<h3>传入的花括号样式：</h3>
       <p>{entryCss}</p>*/}
