3d84edf055dd33a7213ddfe09f0e426eedc39367
AZQD <604972395@qq.com>
2019-11-25 01:42:18 +0800

    所有RN支持的样式key更新

--- src/pages/index/component/AST/utils/rnCollectFilter.js
+++ src/pages/index/component/AST/utils/rnCollectFilter.js
@@ -1,8 +1,9 @@
 /**
  * 所有RN支持的样式key
+ * 依据：https://github.com/vhpoet/react-native-styling-cheat-sheet#image
  * @type {string[]}
  */
-export const rnCollectFilter = [
+/*export const rnCollectFilter = [
   'alignContent',
   'alignItems',
   'alignSelf',
@@ -19,7 +20,6 @@ export const rnCollectFilter = [
   'display',
   'end',
   'start',
-  'flex', // 简写
   'flexDirection',
   'flexBasis',
   'flexGrow',
@@ -28,7 +28,6 @@ export const rnCollectFilter = [
   'height',
   'justifyContent',
   'left',
-  'margin', // 简写
   'marginBottom',
   'marginHorizontal',
   'marginLeft',
@@ -41,7 +40,6 @@ export const rnCollectFilter = [
   'maxWidth',
   'minHeight',
   'minWidth',
-  'padding', // 简写
   'paddingBottom',
   'paddingHorizontal',
   'paddingLeft',
@@ -60,7 +58,6 @@ export const rnCollectFilter = [
   'shadowOpacity',
   'shadowRadius',
   'decomposedMatrix',
-  'transform', // 简写
   'transformMatrix',
   'backfaceVisibility',
   'backgroundColor',
@@ -111,6 +108,10 @@ export const rnCollectFilter = [
   'borderTopEndRadius',
   'borderTopStartRadius',
 
+  'flex', // ok
+  'margin', // 简写ok
+  'padding', // 简写ok
+  'transform', // 简写(直接就是transform)
   'font', // 简写
   'border', // 简写
   'borderLeft', // 简写
@@ -121,6 +122,113 @@ export const rnCollectFilter = [
   'boxShadow', // 简写
   'textShadow', // 简写
   'textDecoration', // 简写
+];*/
+
+/**
+ * 所有RN支持的样式key
+ * 依据：https://www.jianshu.com/p/c59c6890edff
+ * @type {string[]}
+ */
+export const rnCollectFilter = [
+
+  // Text 文本
+  'color',
+  'fontFamily',
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'lineHeight',
+  'textAlign',
+  'textAlignVertical',
+  'textShadowColor',
+  'textShadowOffset',
+
+  // Dimension 尺寸
+  'width',
+  'height',
+
+  // Positioning 定位
+  'position',
+  'top',
+  'right',
+  'bottom',
+  'left',
+
+  // Margin 外部白
+  'margin', // 简写ok
+  'marginHorizontal',
+  'marginVertical',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'marginHorizontal',
+
+  // Padding 内部白
+  'padding', // 简写ok
+  'paddingHorizontal',
+  'paddingVertical',
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+  'paddingHorizontal',
+
+  // Border 边框
+  'borderStyle',
+  'borderWidth',
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+  'borderColor',
+  'borderTopColor',
+  'borderRightColor',
+  'borderBottomColor',
+  'borderLeftColor',
+  'borderRadius',
+  'borderTopLeftRadius',
+  'borderTopRightRadius',
+  'borderBottomLeftRadius',
+  'borderBottomRightRadius',
+  'shadowColor',
+  'shadowOffset',
+
+  // Background 背景
+  'backgroundColor',
+
+  // Transform 转换
+  'transform', // 简写ok
+  'transformMatrix',
+  'backfaceVisibility',
+
+  // Flexbox 弹性盒
+  'flex', // 简写ok
+  'flexDirection',
+  'flexWrap',
+  'justifyContent',
+  'alignItems',
+  'alignSelf',
+
+  // Other 其他
+  'opacity',
+  'overflow',
+  'elevation',
+  'resizeMode',
+  'overlayColor',
+  'tintColor',
+
+  // 简写（加上面的flex、margin、padding、transform）
+  'font', // 简写(直接使用各自的写法，如fontSize等)
+  'border', // 简写ok
+  'borderLeft', // 简写ok
+  'borderRight', // 简写ok
+  'borderTop', // 简写ok
+  'borderBottom', // 简写ok
+  'background', // 简写ok
+  'boxShadow', // 简写//ok
+  'textShadow', // 简写//ok
+  'textDecoration', // 简写//ok
 ];
 
 /**
