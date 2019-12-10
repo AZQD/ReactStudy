f481ff9918b9bcaf6bf21d7a1f77eca8d626bcb3
AZQD <604972395@qq.com>
2019-12-10 11:36:20 +0800

    no-case-declarations

--- src/pages/redux/reducers/handleForm.js
+++ src/pages/redux/reducers/handleForm.js
@@ -9,11 +9,14 @@ function handleForm (state = initState, action) {
   console.log('handleForm：', state, action);
   switch (action.type) {
   case actionData.INPUT_ON_CHANGE:
+    // 加大括号的目的：no-case-declarations：该规则禁止词法声明 (let、const、function 和 class) 出现在 case或default 子句中。
+  {
     const {type, value} = action.payload;
     return {
       ...state,
       [type]: value
     };
+  }
   default:
     return state;
   }
