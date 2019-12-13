95c0968e8cfaffa1dda13ca1d5210d7985839f37
AZQD <604972395@qq.com>
2019-12-13 19:39:02 +0800

    mockjs语法规范

--- src/mockjs/index.js
+++ src/mockjs/index.js
@@ -4,84 +4,9 @@ Mock.mock(
   /\/redux\/demo3\/category\/list/,
   'get',
   {
-    'data': [{
-      'cateId': '1000004',
-      'name': '本地推广',
-      'childList': [{
-        'cateId': '8',
-        'name': '本地推广'
-      }]
-    }, {
-      'cateId': '1000002',
-      'name': '招聘用人',
-      'childList': [{
-        'cateId': '1',
-        'name': '招聘'
-      }]
-    }, {
-      'cateId': '1000003',
-      'name': '房产交易',
-      'childList': [{
-        'cateId': '3',
-        'name': '房屋出租'
-      }, {
-        'cateId': '4',
-        'name': '房屋出售'
-      }]
-    }, {
-      'cateId': '1000005',
-      'name': '车辆交易',
-      'childList': [{
-        'cateId': '17',
-        'name': '车辆出售'
-      }, {
-        'cateId': '18',
-        'name': '车辆求购'
-      }]
-    }, {
-      'cateId': '1000015',
-      'name': '生意转让',
-      'childList': [{
-        'cateId': '5',
-        'name': '生意转让'
-      }]
-    }, {
-      'cateId': '1000011',
-      'name': '打折优惠',
-      'childList': [{
-        'cateId': '31',
-        'name': '打折优惠'
-      }]
-    }, {
-      'cateId': '1000006',
-      'name': '物品交易',
-      'childList': [{
-        'cateId': '14',
-        'name': '出售'
-      }]
-    }, {
-      'cateId': '1000014',
-      'name': '农林牧渔',
-      'childList': [{
-        'cateId': '34',
-        'name': '农作物'
-      }, {
-        'cateId': '35',
-        'name': '畜禽水产'
-      }, {
-        'cateId': '36',
-        'name': '肥药饲料'
-      }, {
-        'cateId': '37',
-        'name': '农机农具'
-      }]
-    }, {
-      'cateId': '1000013',
-      'name': '土特产',
-      'childList': [{
-        'cateId': '33',
-        'name': '土特产'
-      }]
+    'data|10': [{
+      "cateId|+1": 1000001,
+      "name|1": "@province"
     }],
     'msg': '操作成功',
     'code': 0
@@ -92,7 +17,25 @@ Mock.mock(
   /\/redux\/demo3\/submit/,
   'post',
   {
-    'data': null,
+    'data': {
+      "mtime": "@datetime",//随机生成日期时间
+      "score|1-5": 'hello',//随机生成1-5的字符串
+      "rank|1-5":  100,//随机生成1-100的数字
+      "stars|1-5": 5,//随机生成1-5的数字
+      "end|+1": 5,
+      "end|1": true,
+      "obj|2": {
+        "score|1-5": 'hello',
+        "rank|1-5":  100,
+        "stars|1-5": 5,
+      },
+      "arr|6": [{
+        "id|+1": 0,
+        "cateId|+1": 1000001,
+        "name|1": '@province'
+      }],
+      "nickname": "@cname",//随机生成中文名字
+    },
     'msg': '操作成功',
     'code': 0
   }
