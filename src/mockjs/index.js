cc53c4541a518c818a452266bf95a93d26ebd430
AZQD <604972395@qq.com>
2019-12-14 11:19:25 +0800

    mockjs语法规范和数据模板定义示例

--- src/mockjs/index.js
+++ src/mockjs/index.js
@@ -23,77 +23,77 @@ Mock.mock(
   {
     'data': {
       // String
-      "string1|1-10": "★",
-      "string2|3": "★★★",
+      'string1|1-10': '★',
+      'string2|3': '★★★',
 
       // Number
-      "number1|+1": 202,
-      "number2|1-100": 100,
-      "number3|1-100.1-10": 1,
-      "number4|123.1-10": 1,
-      "number5|123.3": 1,
-      "number6|123.10": 1.123,
+      'number1|+1': 202,
+      'number2|1-100': 100,
+      'number3|1-100.1-10': 1,
+      'number4|123.1-10': 1,
+      'number5|123.3': 1,
+      'number6|123.10': 1.123,
 
       // Boolean
       // 'name|1': boolean：随机生成一个布尔值，值为 true 的概率是 1/2，值为 false 的概率同样是 1/2。
-      "boolean2|1": true,
+      'boolean2|1': true,
       // 'name|min-max': value： 随机生成一个布尔值，值为 value 的概率是 min / (min + max)，值为 !value 的概率是 max / (min + max)。
-      "boolean3|1-2": true,
+      'boolean3|1-2': true,
 
       // Object
-      "object1|2": {
-        "310000": "上海市",
-        "320000": "江苏省",
-        "330000": "浙江省",
-        "340000": "安徽省"
+      'object1|2': {
+        '310000': '上海市',
+        '320000': '江苏省',
+        '330000': '浙江省',
+        '340000': '安徽省'
       },
-      "object2|2-4": {
-        "110000": "北京市",
-        "120000": "天津市",
-        "130000": "河北省",
-        "140000": "山西省"
+      'object2|2-4': {
+        '110000': '北京市',
+        '120000': '天津市',
+        '130000': '河北省',
+        '140000': '山西省'
       },
 
       // Array
-      "array1|1": [
-        "AMD",
-        "CMD",
-        "UMD"
+      'array1|1': [
+        'AMD',
+        'CMD',
+        'UMD'
       ],
-      "array2|+1": [
-        "AMD",
-        "CMD",
-        "UMD"
+      'array2|+1': [
+        'AMD',
+        'CMD',
+        'UMD'
       ],
-      "array3|1-10": [
+      'array3|1-10': [
         {
-          "name|+1": [
-            "Hello",
-            "Mock.js",
-            "!"
+          'name|+1': [
+            'Hello',
+            'Mock.js',
+            '!'
           ]
         }
       ],
-      "array4|1-10": [
-        "Mock.js"
+      'array4|1-10': [
+        'Mock.js'
       ],
-      "array5|1-10": [
-        "Hello",
-        "Mock.js",
-        "!"
+      'array5|1-10': [
+        'Hello',
+        'Mock.js',
+        '!'
       ],
-      "array6|3": [
-        "Mock.js"
+      'array6|3': [
+        'Mock.js'
       ],
-      "array7|3": [
-        "Hello",
-        "Mock.js",
-        "!"
+      'array7|3': [
+        'Hello',
+        'Mock.js',
+        '!'
       ],
 
       // Function
       'foo': 'Syntax Demo foo',
-      'fooFun': function() {
+      'fooFun': function () {
         return this.foo
       },
 
