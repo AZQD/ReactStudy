91f9e68a68b9afeca5b11e4e1bfa7e0db14b7305
AZQD <604972395@qq.com>
2020-06-05 16:34:16 +0800

    React路由动态加载方案

--- src/util/loadable.js
+++ src/util/loadable.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 
-export default (loader) => {
-  return Loadable({
-    loader,
-    loading() {
-      return <div>组件加载...</div>
-    },
-  });
-}
+export default (loader) => Loadable({
+  loader,
+  loading () {
+    return <div>组件加载...</div>
+  },
+})
