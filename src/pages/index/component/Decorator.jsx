9dfaa8fb38e19df38a519813afdb3582c0ce70c4
liuchaojie <604972395@qq.com>
2020-07-08 14:44:55 +0800

    update

--- src/pages/index/component/Decorator.jsx
+++ src/pages/index/component/Decorator.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 // 1.类的装饰
 /*function decorator1(target) { // 装饰器函数的第一个参数，就是所要装饰的目标类
   console.log(target);
@@ -27,23 +26,23 @@ console.log('组件的静态属性：', Component1.param2);*/
 
 // 2.方法的装饰
 
-function decorator2 (target, name, descriptor) {
-  console.log(target, name, descriptor);
+function decorator2(target, name, descriptor) {
+    console.log(target, name, descriptor);
 }
 
 export default class Component2 extends React.Component {
-  constructor (porps) {
-    super(porps);
-  }
+    constructor(porps) {
+        super(porps);
+    }
 
-  @decorator2
-  addFun (a, b) {
-    return a + b;
-  }
+    @decorator2
+    addFun(a, b) {
+        return a + b;
+    }
 
-  render () {
-    return <div>类的装饰和方法的装饰</div>
-  }
+    render() {
+        return <div>类的装饰和方法的装饰</div>
+    }
 }
 
 /*
