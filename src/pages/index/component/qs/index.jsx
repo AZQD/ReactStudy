d6e8d905b6db09363e273eb94d55dbbbc7e140bf
AZQD <604972395@qq.com>
2020-11-18 15:29:19 +0800

    qs使用

--- src/pages/index/component/qs/index.jsx
+++ src/pages/index/component/qs/index.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import qs from 'qs';
+
+// 参考文档：https://www.npmjs.com/package/qs
+
+console.log(qs.parse('a=1&b=2')); // {a: "1", b: "2"}
+console.log(qs.parse('?a=1&b=2', {ignoreQueryPrefix: true})); // {a: "1", b: "2"}
+console.log(qs.parse('?a=1;b=2', {ignoreQueryPrefix: true, delimiter: ';'})); // {a: "1", b: "2"}
+
+
+let obj = {c: 3, d: 4};
+console.log(qs.stringify(obj)); // ?c=3&d=4
+console.log(qs.stringify(obj, {addQueryPrefix: true})); // ?c=3&d=4
 
 class Index extends React.Component {
   constructor (props) {
