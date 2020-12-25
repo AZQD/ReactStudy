7d15a2776529694e3e0fb2f4ce943fbb9ea7796a
AZQD <604972395@qq.com>
2020-12-25 16:02:38 +0800

    Loadable

--- src/util/loadable.js
+++ src/util/loadable.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Loadable from 'react-loadable';
 
 export default (loader) => Loadable({
-  loader,
-  loading () {
-    return <div>组件加载中...</div>
-  },
+    loader,
+    loading () {
+        return <div>组件加载中...</div>
+    },
 })
